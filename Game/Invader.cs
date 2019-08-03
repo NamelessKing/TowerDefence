ac353e2cbac81926d19965f7e235c9261bc62eca
namelessking <habibur.tanjin3@gmail.com>
2019-08-04 00:06:59 +0200

    added Invader and Path classes

--- Game/Invader.cs
+++ Game/Invader.cs
@@ -6,5 +6,47 @@ namespace TowerDefence
 {
     class Invader
     {
+        private readonly Path _path;
+        private int _pathStep = 0;
+        public MapLocation Location
+        {
+            get
+            {
+                return _path.GetMapLocationAt(_pathStep);
+            }
+        }
+
+        public int Health { get; private set; } = 2;
+
+        public bool HasScored
+        {
+            get
+            {
+                return _pathStep >= _path.Length;
+            }
+        }
+
+        public bool IsNeutralized => Health <= 0;
+
+        public bool IsActive => !(IsNeutralized || HasScored);
+
+
+
+
+        public Invader(Path path)
+        {
+            _path = path;
+        }
+
+        public void Move()
+        {
+            _pathStep++;
+        }
+
+        public void DecreaseHealth(int factor)
+        {
+            Health -= factor;
+        }
+
     }
 }
--- Game/Path.cs
+++ Game/Path.cs
@@ -6,5 +6,18 @@ namespace TowerDefence
 {
     class Path
     {
+        private readonly MapLocation[] _path;
+        public int Length => _path.Length;
+
+        public Path(MapLocation[] path)
+        {
+            _path = path;
+        }
+
+        public MapLocation GetMapLocationAt(int pathStepIndex)
+        {
+            return (pathStepIndex < _path.Length) ? _path[pathStepIndex] : null;
+        }
+
     }
 }
