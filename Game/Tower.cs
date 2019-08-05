993c8a4f3448f864d419ea31878c1580847de4b2
namelessking <habibur.tanjin3@gmail.com>
2019-08-05 15:13:26 +0200

    added Tower class body

--- Game/Tower.cs
+++ Game/Tower.cs
@@ -6,5 +6,27 @@ namespace TowerDefence
 {
     public class Tower
     {
+
+        private const int _range = 1;
+        private const int _power = 1;
+        private MapLocation _location;
+
+        public Tower(MapLocation location)
+        {
+            _location = location;
+        }
+
+        public void FireOnInvders(Invader[] invaders)
+        {
+            foreach (Invader invader in invaders)
+            {
+                if (invader.IsActive && _location.InRangeOf(invader.Location, _range))
+                {
+                    invader.DecreaseHealth(_power);
+                    break;
+                }
+            }
+        }
+
     }
 }
